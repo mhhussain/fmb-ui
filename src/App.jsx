cadfe4cce5cb359246b1eba8ede3364351f59f66
abdep <abde.poonawala@gmail.com>
2022-12-28 21:21:15 +0000

    Update 4 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Render_count({ count }) {
+  if (count % 2 == 0) {
+    return "count is " + count;
+  } else {
+    return "get rekt";
+  }
+}
 
+function Tasbeeh() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <button onClick={() => setCount(count+1)}>
+      <Render_count count={count} />
+    </button>
   )
 }
 
-export default App
+function ModeManager({ mode }) {
+  switch (mode) {
+  case 0:
+      return <Tasbeeh />
+  case 1:
+      return <Calendar />
+  }
+}
+
+function App() {
+  const [mode, setMode] = useState(0);
+  return (
+    <div className="App">
+      <h1>FMB</h1>
+      <button onClick={() => setMode(1)}>
+        CalendarMode
+      </button>
+      <button onClick={() => setMode(0)}>
+        TasbeehMode
+      </button>
+      <div className="card">
+        <ModeManager mode={mode}/>
+      </div>
+      <p className="footer">
+        © Anjuman-e-Najmi, Detroit 2023
+      </p>
+    </div>
+  );
+}
+
+export default App;
