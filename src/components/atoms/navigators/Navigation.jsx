a7bc33e35391af6a6030d017037b2f3801d88485
mhhussain <2152584+mhhussain@users.noreply.github.com>
2022-12-31 16:50:46 -0500

    Setting up web-deploy and CI/CD (#14)
    * Added static deploy file
    * vite configs and route resolutions for building
    * fixed lock file
    * configs
    * finally working deploy

--- src/components/atoms/navigators/Navigation.jsx
+++ src/components/atoms/navigators/Navigation.jsx
@@ -1,8 +1,8 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from "../../screens/home/Home";
-import Tasbeeh from "../../screens/tasbeeh/Tasbeeh";
-import "../../../styles/index.css";
-import Calendar from "../../screens/calendar/Calendar";
+import Home from "@/screens/home/Home";
+import Tasbeeh from "@/screens/tasbeeh/Tasbeeh";
+import "@/styles/index.css";
+import Calendar from "@/screens/calendar/Calendar";
 const router = createBrowserRouter([
     {
       path: "/",
--- src/components/screens/login/Login.jsx
+++ src/components/screens/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../../styles/Login.css";
+import "@/styles/Login.css";
 
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
