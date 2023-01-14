341424de72fd4410f038362a59049a51397d9cde
mohtamba <59623984+mohtamba@users.noreply.github.com>
2023-01-14 09:46:21 -0800

    Remove unused import

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { api } from '@/api/thaaliApi';
 import { loggedIn } from '@/screens/login/loginSlice';
 
