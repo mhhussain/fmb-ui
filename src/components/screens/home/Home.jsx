d96dda4a460e681072ac7d04f80d9b891dbf4f96
mhhussain <2152584+mhhussain@users.noreply.github.com>
2023-02-12 21:08:59 -0500

    Split week up into fills

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -14,6 +14,11 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineDot from '@mui/lab/TimelineDot';
+import Filter1RoundedIcon from '@mui/icons-material/Filter1Rounded';
+import Filter2RoundedIcon from '@mui/icons-material/Filter2Rounded';
+import Filter4RoundedIcon from '@mui/icons-material/Filter4Rounded';
+import Filter6RoundedIcon from '@mui/icons-material/Filter6Rounded';
+
 import { getWeeklyMenus } from "@/api/services/Menu";
 
 export default function Home() {
@@ -28,50 +33,112 @@ export default function Home() {
   const { memberData } = state; // Read values passed on state
 
   useEffect(() => {
-    getWeeklyMenus(DateTime.now())
-      .then(v => {
-        setState({
-          memberData: state.memberData,
-          menus: v
-        })
-        //state.menus = v;
+    const fetchData = async () => {
+      const lastweek = await getWeeklyMenus(DateTime.now().minus({ weeks: 1 }));
+      const thisweek = await getWeeklyMenus(DateTime.now());
+
+      console.log(weekToFillTimeline(thisweek));
+
+      setState({
+        ...state,
+        lastweek: lastweek,
+        thisweek: thisweek,
       });
-  }, [state]);
+    };
+
+    fetchData();
+  }, []);
+
+  const weekToFillTimeline = (week) => {
+    return week?.reduce((x, menu, i) => {
+      if(!x[menu.fillDate.toFormat('cccc')]) {
+        x[menu.fillDate.toFormat('cccc')] = [];
+      }
+      x[menu.fillDate.toFormat('cccc')].push(menu);
+      return x;
+    }, {})
+  }
 
   return (
     <div className="App">
       <Header />
       <h1>FMB</h1>
+      <Typography><strong>Last Week</strong></Typography>
       <Timeline>
-        {state.menus
-          ?.map(menu => (
-          <TimelineItem key={menu.DailyMenuID}>
-            <TimelineOppositeContent>
-              <Typography>Filling on { menu.fillDate.toFormat('cccc') }</Typography>
-              <Typography>{ menu.date.toLocaleString() }</Typography>
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            CC
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-            <TimelineContent>
-              {menu.items.map(item => (
-                <Typography key={`${menu.DailyMenuID}_${item}`}>{ item }</Typography>
-              ))}
-            </TimelineContent>
-          </TimelineItem>
-        ))}
+        {state.lastweek && Object.values(weekToFillTimeline(state.lastweek))
+          ?.map(menus => (
+            <TimelineItem key={menus[0].DailyMenuID}>
+                <TimelineOppositeContent
+                  sx={{ m: 'auto 0' }}
+                >
+                  <Typography><strong>{ menus[0].fillDate.toFormat('cccc') }</strong></Typography>
+                  <Typography>{ menus[0].date.toLocaleString() }</Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector />
+                  <TimelineDot variant="outlined">
+                    <Filter4RoundedIcon
+                      color="primary"
+                      fontSize="large"
+                    />
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                  {menus?.map(menu =>
+                  (
+                    <div>
+                      <Typography key={menu.DailyMenuID}><strong>{menu.date.toFormat('cccc')}</strong></Typography>
+                      {menu.items.map(item => (
+                        <Typography key={`${menu.DailyMenuID}_${item}`}>{ item }</Typography>
+                      ))}
+                    </div>
+                  ))}
+                </TimelineContent>
+            </TimelineItem>
+          )
+        )}
+      </Timeline>
+      <Typography><strong>This Week</strong></Typography>
+      <Timeline>
+        {state.thisweek && Object.values(weekToFillTimeline(state.thisweek))
+          ?.map(menus => (
+            <TimelineItem key={menus[0].DailyMenuID}>
+                <TimelineOppositeContent
+                  sx={{ m: 'auto 0' }}
+                >
+                  <Typography><strong>{ menus[0].fillDate.toFormat('cccc') }</strong></Typography>
+                  <Typography>{ menus[0].date.toLocaleString() }</Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector />
+                  <TimelineDot variant="outlined">
+                    <Filter4RoundedIcon
+                      color="primary"
+                      fontSize="large"
+                    />
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>
+                  {menus?.map(menu =>
+                  (
+                    <div>
+                      <Typography key={menu.DailyMenuID}><strong>{menu.date.toFormat('cccc')}</strong></Typography>
+                      {menu.items.map(item => (
+                        <Typography key={`${menu.DailyMenuID}_${item}`}>{ item }</Typography>
+                      ))}
+                    </div>
+                  ))}
+                </TimelineContent>
+            </TimelineItem>
+          )
+        )}
       </Timeline>
       <div className="font-black">
         <p>Logged in as {memberData.FirstName} {memberData.LastName}</p>
+        <p>© Anjuman-e-Najmi, Detroit 2023</p>
       </div>
-      <div>
-        <Outlet />
-      </div>
-      <p className="footer">© Anjuman-e-Najmi, Detroit 2023</p>
     </div>
   );
 }
