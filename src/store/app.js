36864585c21f43daef45816215b939240e0cc97e
mhhussain <2152584+mhhussain@users.noreply.github.com>
2024-04-27 01:13:59 -0400

    added vendor and menu item retrieval functions

--- src/store/app.js
+++ src/store/app.js
@@ -5,6 +5,8 @@ export const useAppStore = defineStore('app', {
   state: () => ({
     mumineen: [],
     households: [],
+    vendors: [],
+    menuItems: [],
     thaali: {},
   }),
   actions: {
@@ -20,6 +22,22 @@ export const useAppStore = defineStore('app', {
       try {
         const response = await fetch(`https://us-central1-xyz-moohh-fmbmobile-test.cloudfunctions.net/app/api/v1/lookup/${thaali}`);
         this.thaali = await response.json();
+      } catch (e) {
+        console.error('An error occurred while fetching data: ', e);
+      }
+    },
+    async getVendorList() {
+      try {
+        const response = await fetch(`https://us-central1-xyz-moohh-fmbmobile-test.cloudfunctions.net/app/api/v1/admin/vendor`);
+        this.vendors = await response.json();
+      } catch (e) {
+        console.error('An error occurred while fetching data: ', e);
+      }
+    },
+    async getMenuItemList() {
+      try {
+        const response = await fetch(`https://us-central1-xyz-moohh-fmbmobile-test.cloudfunctions.net/app/api/v1/admin/item`);
+        this.menuItems = await response.json();
       } catch (e) {
         console.error('An error occurred while fetching data: ', e);
       }
