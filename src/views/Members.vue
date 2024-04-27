67fb3215ab911a826b8c63d68507ee83a0fbea32
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2024-04-27 10:46:11 -0400

    added search bar to mumineen view

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,37 +1,73 @@
 <template>
   <v-app>
     Members
+    <v-text-field
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    />
     <v-data-table
       :headers="headers"
-      :items="items"
+      :items="data"
       class="elevation-1"
       :hide-default-footer="true"
       :loading="loading"
+      :search="search"
     >
       <template v-slot:loading>
         <v-skeleton-loader type="table-row@10"></v-skeleton-loader>
+      </template>
+      <template v-slot:item.Edit="{ item }">
+        <v-icon
+          class="me-2"
+          size="small"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
       </template>
     </v-data-table>
   </v-app>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useAppStore } from '@/store/app';
 
 const loading = ref(false);
+const search = ref('');
 
 const store = useAppStore();
 
 const headers = ref([
-  { title: 'ItsID', value: 'ItsID' },
-  { title: 'First Name', value: 'FirstName' },
-  { title: 'Middle Name', value: 'MiddleName' },
-  { title: 'Last Name', value: 'LastName' },
-  { title: 'Cell Phone', value: 'CellPhone' },
+  { title: 'Its ID', value: 'ItsID' },
+  { title: 'HOF', value: 'HOF' },
+  { title: 'Name', value: 'Name' },
+  { title: 'Phone', value: 'Phone' },
   { title: 'Email', value: 'Email' },
+  { title: '', value: 'Edit' },
 ]);
 
+const data = computed(() => {
+  return items.value
+    .map(i => {
+      return {
+        ItsID: i.ItsID,
+        HOF: i.RelationshipToHeadOfHousehold_RelationshipTypeID === 1 ? 'HOF' : '',
+        Name: `${i.Title ? i.Title + ' ' : ''}${i.FirstName} ${i.MiddleName ? i.MiddleName + ' ' : ''}${i.LastName}`,
+        Phone: i.CellPhone,
+        Email: i.Email,
+      };
+    });
+});
+
+const editItem = (item) => {
+  alert('TODO: route to edit mumineen screen');
+};
+
+// Data
 const items = ref([]);
 
 onMounted(async () => {
@@ -43,3 +79,11 @@ onMounted(async () => {
   items.value = store.mumineen;
 });
 </script>
+
+<style scoped>
+.v-text-field {
+  max-height: 75px;
+  max-width: 500px;
+  overflow: hidden;
+}
+</style>
