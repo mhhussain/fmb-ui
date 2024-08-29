f1b6df6b8534518760af56792082b31ced33dfd2
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2024-08-29 00:05:04 -0400

    .

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
     Menu View
+    <v-card v-for="week in weeks">
+      {{ week.weeklyMenuId ``}}
+    </v-card>
   </v-app>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import getWeeksListUseCase from '../domain/usecases/get_weeks_list_usecase';
 
+const weeks = ref([]);
+
 onMounted(async () => {
-  const x = await getWeeksListUseCase.call();
-  console.log(x[0]);
+  const data = await getWeeksListUseCase.call();
+  weeks.value = data;
 });
 </script>
