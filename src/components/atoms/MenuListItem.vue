8aa51a3486436d8b8867b8538c078aa42ee9e8d2
mhhussain <2152584+mhhussain@users.noreply.github.com>
2025-08-01 02:10:56 -0400

    WIP

---
+++ src/components/atoms/MenuListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <n-space justify="space-between" horizontal>
+      <n-text>
+          {{ name }}
+      </n-text>
+      <n-space>
+          <n-text underline>edit</n-text>
+          <n-text underline>remove</n-text>
+      </n-space>
+  </n-space>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/Day.vue
+++ src/views/Day.vue
@@ -8,13 +8,44 @@
                 {{ day }}, {{ date.toFormat('MMM d') }}
             </h1>
         </n-space>
+
+        <n-space class="day-container" vertical>
+            <n-space class="menu-container" vertical>
+                <h3 style="font-weight: bold;">Menu</h3>
+                <n-divider></n-divider>
+                <MenuListItem name="Daal Gosht" />
+                <n-divider></n-divider>
+                <MenuListItem name="Roti" />
+                <n-divider></n-divider>
+                <n-button class="add-menu-item-button" type="info" dashed>
+                    Add Menu Item
+                </n-button>
+                <n-divider></n-divider>
+            </n-space>
+            
+            <n-space class="daily-preferences-container" vertical>
+                <h3 style="font-weight: bold;">Daily Preferences</h3>
+                <n-space horizontal>
+                    <n-text>Group by:</n-text>
+                    <n-button info ghost round>None</n-button>
+                    <n-button info ghost round>Size</n-button>
+                    <n-button info ghost round>Zone</n-button>
+                    <n-button info ghost round>Status</n-button>
+                </n-space>
+                <n-data-table :columns="dailyPreferencesColumns" :data="dailyPreferencesData" :row-class-name="rowClass" />
+            </n-space>
+        </n-space>
     </div>
 </template>
 
 <script setup>
+import { h } from 'vue'
 import { useRoute } from 'vue-router'
 import { DateTime } from 'luxon'
 import { Calendar } from '@vicons/ionicons5'
+import { NButton } from 'naive-ui'
+
+import MenuListItem from '@/components/atoms/MenuListItem.vue'
 
 const route = useRoute();
 const day = route.params.day.charAt(0).toUpperCase() + route.params.day.slice(1);
@@ -39,7 +70,145 @@ switch (day) {
     case 'Saturday':
         date = DateTime.fromFormat(route.params.startDate, 'yyyyLLdd').plus({ days: 5 });
         break;
+};
+
+const dailyPreferencesColumns = [
+    {
+        key: 'no',
+        title: 'No.',
+        width: 30
+    },
+    {
+        key: 'size',
+        title: 'Size',
+        className: 'size',
+        width: 30
+    },
+    {
+        key: 'zone',
+        title: 'Zone',
+        width: 50,
+        ellipsis: {
+            tooltip: true
+        }
+    },
+    {
+        key: 'household',
+        title: 'Household',
+        width: 100,
+        ellipsis: {
+            tooltip: true
+        }
+    },
+    {
+        key: 'status',
+        title: 'Status',
+        className: 'status',
+        width: 50
+    },
+    {
+        key: 'edit',
+        title: '',
+        width: 25,
+        render: (row) => {
+            return h(NButton, {
+                size: 'small',
+                text: true,
+                onClick: () => {
+                    console.log(row)
+                },
+            }, {
+                default: () => 'Edit'
+            })
+        }
+    }
+];
+
+const rowClass = (row) => {
+    const classes = [];
+    if (row.status === 'Missing') {
+        classes.push('missing-thaali');
+    }
+    if (row.default) {
+        classes.push('default-preference');
+    }
+    return classes;
 }
+
+const dailyPreferencesData = [
+    {
+        no: 57,
+        size: 'B',
+        default: true,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Missing',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+    {
+        no: 97,
+        size: 'C',
+        default: true,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Missing',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+    {
+        no: 57,
+        size: 'B',
+        default: false,
+        zone: 'F. Hills Zone 4',
+        household: 'Shk. Mustafa Hussain',
+        status: 'Checked in',
+    },
+]
 
 </script>
 
@@ -48,5 +217,42 @@ switch (day) {
     display: flex;
     align-items: end;
     padding-bottom: 10px;
+}
+
+.day-container {
+    padding: 24px;
+    border: 2px solid #E7E7E7;
+    border-radius: 2px;
+}
+
+.menu-container {
+    max-width: 50%;
+    padding-bottom: 24px;
+}
+
+.daily-preferences-container {
+    /* max-width: 70%; */
+}
+
+.n-divider {
+    margin: 0;
+}
+
+.add-menu-item-button {
+    width: 100%;
+}
+
+:deep(.size) {
+    font-weight: bold;
+}
+
+:deep(.missing-thaali .status) {
+    color: red;
+    font-weight: bold;
+}
+
+:deep(.default-preference .size) {
+    font-weight: normal;
+    font-style: italic;
 }
 </style>
