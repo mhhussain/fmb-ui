39be31cb2003512b73680056e0d42bbb3e45ce9a
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2025-08-04 12:05:05 -0400

    Enhancement
    Cleaning up day view and moved menu list to new component

---
+++ src/components/organisms/MenuList.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3 style="font-weight: bold;">Menu</h3>
+    <n-divider></n-divider>
+    <MenuListItem v-for="item in menu" :key="item.dailyMenuItemId" :name="item.description" />
+    <n-divider></n-divider>
+    <n-button class="add-menu-item-button" type="info" dashed>
+        Add Menu Item
+    </n-button>
+    <n-divider></n-divider>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import MenuListItem from '@/components/molecules/MenuListItem.vue';
+
+const props = defineProps({
+    menu: {
+        type: Array,
+        required: true
+    }
+});
+
+const menu = computed(() => props.menu);
+</script>
+
+<style scoped>
+.add-menu-item-button {
+    width: 100%;
+}
+</style>
